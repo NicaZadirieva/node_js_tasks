c498cfc1a1d1994e0ac20bae565140eeb5e8fb0f
Veronica <no.demchenko@yandex.ru>
2025-02-27 10:51:42 +0300

    refactor(3-calc): вынесла в initCli

--- 3-calc/index.js
+++ 3-calc/index.js
@@ -1,25 +1,32 @@
 const { add } = require("./add");
 const { multiply } = require("./multiply");
 
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
+const METHODS = ["add", "multiply"];
+const [ADD_METHOD, MULTIPLY_METHOD] = METHODS;
 
-if (!a || !b || isNaN(a) || isNaN(b)) {
-  console.error("Please provide two numbers as an argument");
-  process.exit(1);
+function initCli() {
+  const a = Number(process.argv[2]);
+  const b = Number(process.argv[3]);
+
+  if (!a || !b || isNaN(a) || isNaN(b)) {
+    console.error("Please provide two numbers as an argument");
+    process.exit(1);
+  }
+
+  const method = process.argv[4];
+
+  if (!METHODS.includes(method)) {
+    console.error("Please provide 'add' or'multiply' as a third argument");
+    process.exit(1);
+  }
+
+  if (method === ADD_METHOD) {
+    console.log(add(a, b));
+  }
+
+  if (method === MULTIPLY_METHOD) {
+    console.log(multiply(a, b));
+  }
 }
 
-const method = process.argv[4];
-
-if (!method || (method !== "add" && method !== "multiply")) {
-  console.error("Please provide 'add' or'multiply' as a third argument");
-  process.exit(1);
-}
-
-if (method === "add") {
-  console.log(add(a, b));
-}
-
-if (method === "multiply") {
-  console.log(multiply(a, b));
-}
+initCli();
