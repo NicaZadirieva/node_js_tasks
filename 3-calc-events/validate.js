3ffd109f620d8ed010c62624bcd51402a97d2edb
Veronica <no.demchenko@yandex.ru>
2025-02-27 10:39:12 +0300

    refactor(3-calc-events): вынесла методы
    вынесла методы в константу

--- 3-calc-events/validate.js
+++ 3-calc-events/validate.js
@@ -1,10 +1,11 @@
+const METHODS = ["add", "multiply"];
 function validate(a, b, method) {
   if (!a || !b || isNaN(a) || isNaN(b)) {
     throw new Error("Please provide two numbers as an argument");
   }
 
-  if (!method || (method !== "add" && method !== "multiply")) {
-    throw new Error("Please provide 'add' or'multiply' as a third argument");
+  if (!METHODS.includes(method)) {
+    throw new Error("Please provide correct method as a third argument");
   }
 
   return true; // validation successful
