2c0b1d4e92eb90298424a0a8b5b44064ba9e6c2b
Veronica <no.demchenko@yandex.ru>
2025-02-27 10:50:59 +0300

    refactor(3-calc-events): вынесла в initCli

--- 3-calc-events/index.js
+++ 3-calc-events/index.js
@@ -4,39 +4,38 @@ const { EventEmitter } = require("events");
 const { validate } = require("./validate");
 
 const myEmitter = new EventEmitter();
-
+const METHODS = ["add", "multiply"];
+const [ADD_METHOD, MULTIPLY_METHOD] = METHODS;
+const RESULT_TYPE = "result";
+const ERROR_TYPE = "error";
 // event handlers while success
-myEmitter.on("result", (result) => {
+myEmitter.on(RESULT_TYPE, (result) => {
   console.log(`Result: ${result}`);
 });
-myEmitter.on("add", (a, b) => {
-  myEmitter.emit("result", add(a, b));
+myEmitter.on(ADD_METHOD, (a, b) => {
+  myEmitter.emit(RESULT_TYPE, add(a, b));
 });
 
-myEmitter.on("multiply", (a, b) => {
-  myEmitter.emit("result", multiply(a, b));
+myEmitter.on(MULTIPLY_METHOD, (a, b) => {
+  myEmitter.emit(RESULT_TYPE, multiply(a, b));
 });
 
 // event handlers while error
-myEmitter.on("error", (error) => {
+myEmitter.on(ERROR_TYPE, (error) => {
   console.error(`Error: ${error.message}`);
   process.exit(1);
 });
 
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-const method = process.argv[4];
-
-try {
-  validate(a, b, method);
-
-  if (method === "add") {
-    myEmitter.emit("add", a, b);
+function initCli() {
+  try {
+    const a = Number(process.argv[2]);
+    const b = Number(process.argv[3]);
+    const method = process.argv[4];
+    validate(a, b, method);
+    myEmitter.emit(method, a, b);
+  } catch (e) {
+    myEmitter.emit(ERROR_TYPE, e);
   }
-
-  if (method === "multiply") {
-    myEmitter.emit("multiply", a, b);
-  }
-} catch (e) {
-  myEmitter.emit("error", e);
 }
+
+initCli();
