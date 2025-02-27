c57870d62de63867b28b77f5f032eff2dbf88873
Veronica <no.demchenko@yandex.ru>
2025-02-27 10:51:58 +0300

    refactor(4-timer): вынесла в initCli

--- 4-timer/app.js
+++ 4-timer/app.js
@@ -1,15 +1,17 @@
 const utilTimer = require("./timer");
 
-const time = process.argv.slice(2).join(" ");
-console.log(time);
+function initCli() {
+  const time = process.argv.slice(2).join(" ");
+  if (!time) {
+    console.error("Please provide a time duration.");
+    process.exit(1);
+  }
+  try {
+    utilTimer.createTimer(time, "Time is up!");
+  } catch (err) {
+    console.error("Invalid time duration format.");
+    process.exit(1);
+  }
+}
 
-if (!time) {
-  console.error("Please provide a time duration.");
-  process.exit(1);
-}
-try {
-  utilTimer.createTimer(time, "Time is up!");
-} catch (err) {
-  console.error("Invalid time duration format.");
-  process.exit(1);
-}
+initCli();
