c264073a92c6fadc86ffd928674291fbe71598c4
Veronica <no.demchenko@yandex.ru>
2025-03-06 14:16:37 +0300

    feat: добавила получение аргументов

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -1,1 +1,21 @@
 #!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+const initCli = () => {
+  console.log("CLI started...", process.argv);
+  const args = getArgs(process.argv);
+  console.log(args);
+
+  if (args.h) {
+    // Вывод help
+  }
+
+  if (args.s) {
+    // Сохранить город
+  }
+
+  if (args.t) {
+    // Сохранить токен
+  }
+};
+
+initCli();
