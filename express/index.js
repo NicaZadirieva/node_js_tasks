233e4765351da18bdbb030ab57a171b262050aba
Veronica <no.demchenko@yandex.ru>
2025-03-16 12:10:59 +0300

    feat: динамический роут

--- express/index.js
+++ express/index.js
@@ -4,10 +4,15 @@ const port = 8000;
 const host = "localhost";
 const app = express();
 
-app.get("/hello", (req, res) => {
+app.all("/hello", (req, res, next) => {
+  console.log("Hello, world!");
+  next();
+});
+
+app.get("/hel?lo", (req, res) => {
   res.status(200);
   res.header("Content-Type", "text/plain");
-  res.send("Hello, World!");
+  res.send("Hello, Express!");
 });
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port}/`);
