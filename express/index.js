9d0bd14cf378aee220c747c9ad43d09801679737
Veronica <no.demchenko@yandex.ru>
2025-03-16 15:19:47 +0300

    feat: пример с роутером

--- express/index.js
+++ express/index.js
@@ -1,11 +1,10 @@
 import express from "express";
-import router from "./test.js";
-
+import { router as userRouter } from "./users/users.js";
 const port = 8000;
 const host = "localhost";
 const app = express();
 
-app.use("/user", router);
+app.use("/users", userRouter);
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
