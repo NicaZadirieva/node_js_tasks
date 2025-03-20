c4ffab3a94c60efb2b176c6239cc6c6d48a562f7
Veronica <no.demchenko@yandex.ru>
2025-03-20 22:39:40 +0300

    feat: 1-ый вариант реализации DI

--- dashboard-api/src/app.ts
+++ dashboard-api/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
 export class App {
     app: Express;
     port: number;
     server?: Server;
+    logger?: LoggerService;
 
     constructor() {
         this.app = express();
@@ -18,7 +20,8 @@ export class App {
     public async init() {
         this.useRoutes();
         this.server = await this.app.listen(this.port);
+        this.logger = new LoggerService();
         //TODO: логгер
-        console.log(`Server is running on port ${this.port}`);
+        this.logger.log(`Server is running on port ${this.port}`);
     } 
 }
