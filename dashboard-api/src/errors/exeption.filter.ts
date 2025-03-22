3284c1587dceffd93190df7ee5189351c6fa5423
Veronica <no.demchenko@yandex.ru>
2025-03-22 22:56:56 +0300

    fix(errors): добавила объект ошибки в send

--- dashboard-api/src/errors/exeption.filter.ts
+++ dashboard-api/src/errors/exeption.filter.ts
@@ -3,12 +3,12 @@ import { LoggerService } from '../logger/logger.service';
 import { IExceptionFilter } from './exception.filter.interface';
 import { HTTPError } from './http-error.class';
 
-export class ExceptionFilter implements IExceptionFilter{
+export class ExceptionFilter implements IExceptionFilter {
     constructor(private logger: LoggerService) {}
     catch (error: Error | HTTPError, request: Request, response: Response, next: NextFunction) {
         if (error instanceof HTTPError) {
             this.logger.error(`[${error.context}] ${error.statusCode} : Ошибка ${error.message}`);
-            response.status(error.statusCode || 500).send();
+            response.status(error.statusCode || 500).send({ err: error.message });
         } else {
             this.logger.error(`${error.message}`);
             response.status(500).send({ err: error.message });
