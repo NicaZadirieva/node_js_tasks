696ab76bfaa0957d7de991982b2ef1bea38df87a
Veronica <no.demchenko@yandex.ru>
2025-04-15 13:19:04 +0300

    feat: тесты на логин

--- dashboard-api/tests/users.e2e-spec.ts
+++ dashboard-api/tests/users.e2e-spec.ts
@@ -13,9 +13,24 @@ describe('Users e2e', () => {
     it('Register - error', async () => {
         const res = await request(application.app)
         .post('/users/register')
-        .send({ email: 'a@a.ru', password: '1'});
+        .send({ email: 'a@a.ru', password: '1', name: 'Nica'});
         expect(res.statusCode).toBe(422);
     });
+    it('Login - success', async () => {
+        const res = await request(application.app)
+        .post('/users/login')
+        .send({ email: 'a@a.ru', password: '1', name: 'Nica'});
+        expect(res.statusCode).toBe(200);
+        expect(res.body.jwt).not.toBe(undefined);
+    });
+    it('Login - failed', async () => {
+        const res = await request(application.app)
+        .post('/users/login')
+        .send({ email: 'a@a.ru', password: '123', name: 'Nica'});
+        expect(res.statusCode).toBe(401);
+        expect(res.body.jwt).toBe(undefined);
+    });
+
 
 })
 
